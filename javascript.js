9c7cfdf330bc0763459df5b27d1b6b8d715c3889
mrubin01 <mi.rubino@protonmail.com>
2025-09-03 14:15:03 +0100

    Added a break in case of wrong human input

--- javascript.js
+++ javascript.js
@@ -10,24 +10,24 @@ function getHumanChoice() {
 function playRound(computerChoice, humanChoice) {
         if (computerChoice.toUpperCase() === humanChoice.toUpperCase()) {
             console.log("DRAW!!!")
-            return "draw"
+            return "draw";
         } else if (computerChoice.toUpperCase() === "ROCK" && humanChoice.toUpperCase() === "PAPER") {
             console.log("Rock vs Paper: HUMAN WINS!!!")
-            return "human"
+            return "human";
         } else if (computerChoice.toUpperCase() === "ROCK" && humanChoice.toUpperCase() === "SCISSORS") {
             console.log("Rock vs Scissors: COMPUTER WINS!!!")
-            return "computer"
+            return "computer";
         } else if (computerChoice.toUpperCase() === "PAPER" && humanChoice.toUpperCase() === "ROCK") {
             console.log("Paper vs Rock: COMPUTER WINS!!!")
-            return "computer"
+            return "computer";
         } else if (computerChoice.toUpperCase() === "PAPER" && humanChoice.toUpperCase() === "SCISSORS") {
             console.log("Paper vs Scissors: HUMAN WINS!!!")
-            return "human"
+            return "human";
         } else if (computerChoice.toUpperCase() === "SCISSORS" && humanChoice.toUpperCase() === "PAPER") {
             console.log("Scissors vs Paper: COMPUTER WINS!!!")
-            return "computer"
+            return "computer";
         } else if (computerChoice.toUpperCase() === "SCISSORS" && humanChoice.toUpperCase() === "ROCK") {
             console.log("Scissors vs Rock: HUMAN WINS!!!")
-            return "human"
+            return "human";
         }
     }
